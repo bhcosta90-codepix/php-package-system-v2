b139a8683bab6a55f4cf427d7d31c11caed80245
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-26 12:21:52 -0300

    wip

--- src/Domain/DomainPixKey.php
+++ src/Domain/DomainPixKey.php
@@ -6,13 +6,25 @@ namespace CodePix\System\Domain;
 
 use CodePix\System\Domain\Enum\EnumPixType;
 use Costa\Entity\Data;
+use Costa\Entity\ValueObject\Uuid;
 
 class DomainPixKey extends Data
 {
     public function __construct(
         protected EnumPixType $kind,
-        protected string $key,
+        protected ?string $key,
     ) {
+        if ($this->kind == EnumPixType::ID && empty($this->key)) {
+            $this->key = (string)Uuid::make();
+        }
+
         parent::__construct();
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'key' => 'required',
+        ];
     }
 }
--- tests/Unit/Domain/DomainPixKeyTest.php
+++ tests/Unit/Domain/DomainPixKeyTest.php
@@ -6,6 +6,7 @@ use CodePix\System\Domain\DomainPixKey;
 use CodePix\System\Domain\Enum\EnumPixType;
 
 use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertNotNull;
 
 describe("DomainPixKey Unit Tests", function () {
     test("creating a new transaction", function () {
@@ -21,6 +22,20 @@ describe("DomainPixKey Unit Tests", function () {
             'created_at' => $entity->createdAt(),
             'updated_at' => $entity->updatedAt(),
         ], $entity->toArray());
+
+        $entity = new DomainPixKey(
+            kind: EnumPixType::ID,
+            key: '4393e8bc-73f7-11ee-b962-0242ac120002',
+        );
+
+        assertEquals('4393e8bc-73f7-11ee-b962-0242ac120002', $entity->key);
+
+        $entity = DomainPixKey::make(
+            kind: EnumPixType::ID,
+            key: '4393e8bc-73f7-11ee-b962-0242ac120002',
+        );
+
+        assertEquals('4393e8bc-73f7-11ee-b962-0242ac120002', $entity->key);
     });
 
     test("making a transaction", function () {
@@ -55,5 +70,14 @@ describe("DomainPixKey Unit Tests", function () {
             'created_at' => '2020-01-01 00:00:00',
             'updated_at' => '2020-01-01 00:00:00',
         ], $entity->toArray());
+    });
+
+    test("creating a new pix key with type id", function () {
+        $entity = new DomainPixKey(
+            kind: EnumPixType::ID,
+            key: null,
+        );
+
+        assertNotNull($entity->key);
     });
 });
